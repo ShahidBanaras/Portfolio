a16983426a174b196af8b5dd978c51b642b506df
Shahid banaras <152716364+ShahidBanaras@users.noreply.github.com>
2024-03-12 15:57:53 +0000

    set home

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Shahid Banaras",
+  title: "Python experience, Cloud Computing, and Data Engineering enthusiast",
+  email: "shahidbanaras256@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "shahidbanaras",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
